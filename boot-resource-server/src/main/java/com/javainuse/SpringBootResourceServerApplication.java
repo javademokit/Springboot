58a474519c2d1f810ceecba4be8f6c429e6637a0
Traininghubshyamlalyadav1990 <ajesh.yadav122002@gmail.com>
2020-05-24 21:00:43 +0530

    spring-boot auth server

---
+++ boot-resource-server/src/main/java/com/javainuse/SpringBootResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootResourceServerApplication.class, args);
+    }
+}
---
+++ boot-resource-server/src/main/java/com/javainuse/controllers/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.javainuse.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.javainuse.model.Employee;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/user/getEmployeesList", produces = "application/json")
+    @ResponseBody
+    public List<Employee> getEmployeesList() {
+        List<Employee> employees = new ArrayList<>();
+        Employee emp = new Employee();
+        emp.setEmpId("emp1");
+        emp.setEmpName("emp1");
+        employees.add(emp);
+        return employees;
+
+    }
+
+}
